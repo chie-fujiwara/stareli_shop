48aa5ef61c00454e7e5ba715bc7f4c74a34d6ea2
Naoto Shimoda <n104075n@gmail.com>
2019-09-15 19:20:54 +0900

    modelとそれらに対応するテーブルのマイグレーションファイルを作成

---
+++ database/migrations/2019_09_15_085248_create_work_schedules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_schedules', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_schedules');
+    }
+}
