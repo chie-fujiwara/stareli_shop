bef243d105a375c6952420dc298635c20944e6d9
Naoto Shimoda <n104075n@gmail.com>
2019-09-15 19:17:27 +0900

    Usersカラム追加[マイグレーションファイル]

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('name_kana');
             $table->string('sex');
+            $table->string('birthday');
             $table->string('telnum');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
